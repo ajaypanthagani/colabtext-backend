46d99c75ef7d753bcb5bff565e14a7a5dea02f37
ajay.panthagani <ajaypanthagani321@gmail.com>
2025-05-17 12:31:43 +0530

    ping the client periodically to test for inactivity

--- handler/document/document.go
+++ handler/document/document.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
@@ -34,17 +35,45 @@ func (h *DocumentHandler) Write(ctx *gin.Context) {
 
 	defer conn.Close()
 
+	// Set initial read deadline
+	conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+
+	// Setup pong handler to reset read deadline on pong receipt
+	conn.SetPongHandler(func(appData string) error {
+		conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+		return nil
+	})
+
+	// Start goroutine to send periodic pings to client
+	go func() {
+		ticker := time.NewTicker(30 * time.Second)
+		defer ticker.Stop()
+
+		for {
+			<-ticker.C
+			if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				log.Println("Ping failed:", err)
+				return
+			}
+		}
+	}()
+
+	// Read message loop
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("Error reading message:", err)
 			break
 		}
+
 		log.Printf("Received message from clientID: %s: clientName: %s: %s\\n", clientID, clientName, message)
+
 		// Echo the message back to the client
 		if err := conn.WriteMessage(websocket.TextMessage, message); err != nil {
 			log.Println("Error writing message:", err)
 			break
 		}
 	}
+
+	log.Println("WebSocket connection closed cleanly")
 }
